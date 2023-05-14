69d03b049df029e652734cdc593cbf261f61d2a2
Nemexia <amirhossein.rahmani55@gmail.com>
2023-05-15 00:07:41 +0430

    formated

--- PlaySync.py
+++ PlaySync.py
@@ -1,23 +1,27 @@
-#file names
+# file names
+import threading
+import requests
+import keyboard
+import time
+import vlc
+
+
+# imports
 movie_name = '11.mp4'
 subtitle_name = '11.srt'
 
-#imports
-import vlc
-import time
-import keyboard
-import requests
-import threading
-
-
-#server functions
+# server functions
 def set_data(time):
-    requests.get(f'https://nemspace.ir/syncplay/withJQ/set_data.php?time={time}')
+    requests.get(
+        f'https://nemspace.ir/syncplay/withJQ/set_data.php?time={time}')
+
+
 def get_data():
     text = requests.get(f'https://nemspace.ir/syncplay/withJQ/get_data.php')
     return int(text.text)
- 
-#initialization
+
+
+# initialization
 instance = vlc.Instance()
 player = instance.media_player_new()
 media = instance.media_new(movie_name)
@@ -29,42 +33,42 @@ player.pause()
 player.set_time(0)
 set_data(-2)
 
-#player thread function
+# player thread function
 def movie_thread_function():
     while True:
         current_status = get_data()
-        if current_status == -1:                     #server playing
-            if media.get_state() != 3:               #pc not playing
-                player.set_pause(0)                  #play pc
-        else:                                        #server paused
-            if current_status != -2:                 #at some value
-                player.set_time(current_status)      #go there
-            if media.get_state() != 4:               #pc not paused
-                player.set_pause(1)                  #pause pc
-            
+        if current_status == -1:  # server playing
+            if media.get_state() != 3:  # pc not playing
+                player.set_pause(0)  # play pc
+        else:  # server paused
+            if current_status != -2:  # at some value
+                player.set_time(current_status)  # go there
+            if media.get_state() != 4:  # pc not paused
+                player.set_pause(1)  # pause pc
+
 
 def main():
-    movie_thread = threading.Thread(target=movie_thread_function) 
+    movie_thread = threading.Thread(target=movie_thread_function)
     movie_thread.start()
     already_pressed = False
     while True:
         if keyboard.is_pressed("space"):
-            #pause key
+            # pause key
             if already_pressed == False:
-                if get_data() == -1:     #playing
-                    set_data(-2)         #pause
+                if get_data() == -1:  # playing
+                    set_data(-2)  # pause
                 else:
-                    set_data(-1)         #playing
+                    set_data(-1)  # playing
             already_pressed = True
         elif keyboard.is_pressed("right"):
-            #+5s jump key
+            # +5s jump key
             if already_pressed == False:
                 current_time = player.get_time()
                 new_time = current_time+5000
                 set_data(new_time)
             already_pressed = True
         elif keyboard.is_pressed("left"):
-            #-5s jump key
+            # -5s jump key
             if already_pressed == False:
                 current_time = player.get_time()
                 new_time = current_time-5000
@@ -72,6 +76,7 @@ def main():
             already_pressed = True
         else:
             already_pressed = False
+
 
 if __name__ == "__main__":
     main()
